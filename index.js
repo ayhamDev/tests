d0274b040ffebe527cf22acb2612ab0219f80e34
ayham <ayhamgm81@gmail.com>
2023-08-22 11:37:30 +0300

    next

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
 const express = require("express");
-const pm2 = require("pm2");
+
 require("cors")();
 
 const app = express();
 app.use(require("express-status-monitor")());
 app.get("/", (req, res) => {
-  res.json({ msg: process.env.NODE_ENV, msg: "hello" });
+  res.json({ msg: process.env.NODE_ENV, msg: "hello", type: "fis" });
   console.log(req);
 });
-
 app.get("/admin", (req, res) => {});
 app.listen(4000, () => {
   console.log("Server Running on >>> http://localhost:4000");
--- pm2.js
+++ pm2.js
@@ -1,5 +1,33 @@
 const pm2 = require("pm2");
+const async = require("async");
+pm2.list((err, procs) => {
+  async.forEachLimit(procs, 1, (proc, next) => {
+    console.log(proc.pm2_env.versioning);
+    if (proc.pm2_env && proc.pm2_env.versioning) {
+      console.log("pull And Reload %s", proc.name);
 
-pm2.list((err, list) => {
-  console.log(list);
+      pm2.reload(proc.name, (err, meta) => {
+        if (meta) {
+          var rev = meta.rev;
+
+          if (rev)
+            console.log(
+              "Successfully pulled [App name: %s] [Commit id: %s] [Repo: %s] [Branch: %s]",
+              proc.name,
+              rev.current_revision,
+              meta.procs[0].pm2_env.versioning.repo_path,
+              meta.procs[0].pm2_env.versioning.branch
+            );
+          else {
+            // Backward compatibility
+            console.log("App %s succesfully pulled");
+          }
+        }
+
+        if (err) console.log("App %s already at latest version", proc.name);
+
+        return next();
+      });
+    } else next();
+  });
 });
